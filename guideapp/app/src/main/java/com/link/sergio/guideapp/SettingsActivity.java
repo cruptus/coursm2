7cbcde988c8d3f9c7b9a943784c7e70a2183b805
Jeremie Elbaz <jeremie@MacBook-Pro-de-Jeremie.local>
2017-11-21 11:46:12 +0100

    ajout de l'evenement checkbox

--- guideapp/app/src/main/java/com/link/sergio/guideapp/SettingsActivity.java
+++ guideapp/app/src/main/java/com/link/sergio/guideapp/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.link.sergio.guideapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 
 import java.util.List;
@@ -12,6 +14,42 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_settings);
+
+        this.initEventCheckBox();
+
+    }
+
+    private void initEventCheckBox() {
+        final CheckBox checkboxEnKm = ( CheckBox ) findViewById( R.id.checkboxEnKm );
+        final CheckBox checkboxEnMile = ( CheckBox ) findViewById( R.id.checkboxEnMile );
+
+        checkboxEnKm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if ( isChecked )
+                {
+                    checkboxEnMile.setChecked(false);
+                }
+
+            }
+        });
+
+        checkboxEnMile.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if ( isChecked )
+                {
+                    checkboxEnKm.setChecked(false);
+                }
+
+            }
+        });
+
     }
 }
