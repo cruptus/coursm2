847ff2ea56374537cf11559b94314db1ef74a678
Ceccema <manicacci.fm.pro@gmail.com>
2017-11-21 11:04:29 +0100

    New version of JSON Parser

--- guideapp/app/src/main/java/data/Parser.java
+++ guideapp/app/src/main/java/data/Parser.java
@@ -1,12 +1,14 @@
 package data;
 
-import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by francois-mariemanicacci on 14/11/2017.
  */
 
 public interface Parser {
-    String getTitle();
-    String getDescription();
+    List<Map<String, String>> getItems () throws JSONException;
 }
