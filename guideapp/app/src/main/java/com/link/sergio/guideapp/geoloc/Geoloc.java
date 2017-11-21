dd37ca289298e6c04f02123a7a65d099f6c24f3a
ARAUJO David <d.araujo2b@gmail.com>
2017-11-21 10:26:31 +0100

    Classe geoloc

---
+++ guideapp/app/src/main/java/com/link/sergio/guideapp/geoloc/Geoloc.java
@@ -0,0 +1,72 @@
+package com.link.sergio.guideapp.geoloc;
+
+import android.location.*;
+import android.os.Bundle;
+
+/**
+ * @author ARAUJO David
+ *
+ * Classe Geoloc permettant de récupérer les données de géolocalisation du téléphone utilisant l'application.
+ */
+
+public class Geoloc implements LocationListener {
+
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private float  accuracy;
+    private int    status;
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.latitude  = location.getLatitude();
+        this.longitude = location.getLongitude();
+        this.altitude  = location.getAltitude();
+        this.accuracy  = location.getAccuracy();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        this.status = status;
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public float getAccuracy() {
+        return accuracy;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * Fonction getLonLat
+     * Les valeurs numériques de longitude et de latitude peuvent être obtenues avec les fonctions getLatitude() et getLongitude().
+     * @return La longitude et la atitude sous forme de texte au format JSON.
+     */
+
+    public String getLonLat() {
+        return "{lon:" + this.longitude + ",lat:" + this.latitude + "}";
+    }
+}
