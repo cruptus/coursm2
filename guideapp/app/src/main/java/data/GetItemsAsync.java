ead4cdda2615896dc3eb3ce4814fc35a7666a9fc
sergio <miranda@goodbarber.com>
2017-11-21 14:06:57 +0100

    Async commit

---
+++ guideapp/app/src/main/java/data/GetItemsAsync.java
@@ -0,0 +1,17 @@
+package data;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by sergio on 21/11/2017.
+ */
+
+public class GetItemsAsync extends AsyncTask<Void, Void, Boolean>
+{
+
+    @Override
+    protected Boolean doInBackground(Void... voids)
+    {
+        return null;
+    }
+}
